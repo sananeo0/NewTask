3c14dbf87d8836c85db9cfd8e82a7ae45e211a88
sananeo0 <sananeo@code.edu.az>
2023-08-29 14:41:53 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿namespace NewTask
+{
+    public class Program
+    {
+        public static bool CustomTryParse(string input, out int a)
+        {
+            try
+            {
+                a = int.Parse(input);
+                return true;
+            }
+            catch (FormatException)
+            {
+                a = 0;
+                return false;
+                
+            }
+                
+        }
+        static void Main(string[] args)
+        {
+            if( CustomTryParse("19", out int a))
+            {
+                Console.WriteLine(a);
+            }
+            else
+            {
+                Console.WriteLine("Failed");
+            }
+
+
+
+        }
+    }
+}
